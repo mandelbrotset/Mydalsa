bfa574ba9310d1e6dc8eaa4f44e19334cdafeb39
Linus Lindgren <linlind@student.chalmers.se>
2014-09-06 02:52:04 +0200

    Now shows texture from tileMap almost correct. (Some clipping issues still exists though.. )

--- MyFlaxa/src/com/mydalsa/myflaxa/states/GameState.java
+++ MyFlaxa/src/com/mydalsa/myflaxa/states/GameState.java
@@ -1,11 +1,21 @@
 package com.mydalsa.myflaxa.states;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
@@ -18,108 +28,171 @@ import com.mydalsa.myflaxa.MyFlaxaGame;
 import com.mydalsa.myflaxa.entities.Sprite;
 
 public class GameState extends State {
-	public static final float PPM = 100.0f;
+	public static final float PPM = 1000f;
+	public static final float GRAVITY = -9.82f;
 	
+	public static final float BIRD_WEIGHT = 10;
+
 	private SpriteBatch batch;
-	
-	private boolean debug = false;
+
+	private boolean debug = true;
 	private World world;
 	private Box2DDebugRenderer b2dr;
 	private OrthographicCamera cam;
 	private Sprite player;
 	private Matrix4 debugMatrix;
 	private Body body;
-
+	private OrthogonalTiledMapRenderer tmr;
+	private TiledMap map;
 	
+	private Vector3 eye = new Vector3(1000000f/PPM, 1000000f/PPM, 0f);
+	private int zoom = 10;
+
+
 	public GameState(MyFlaxaGame game) {
 		super(game);
 		batch = game.getSpriteBatch();
 		cam = game.getCamera();
-		
+
 		// set up box2d stuff
-		world = new World(new Vector2(0, -9.82f), true);
-	//	world.setContactListener(cl = new MyContactListener());
+		world = new World(new Vector2(0, GRAVITY), true);
+
 		b2dr = new Box2DDebugRenderer();
-	
+
 		// create player
 		createPlayer();
-		
+
 		// create tiles
-//		createTiles();	
-		createGround();
-		
-		
+		loadTiles();
+
 		// set up debug matrix
 		debugMatrix = new Matrix4(cam.combined);
 		debugMatrix.scl(PPM);
-		/*b2dCam = new OrthographicCamera();
-		b2dCam.setToOrtho(false, Game.V_WIDTH / PPM, Game.V_HEIGHT / PPM);*/
-	
+
 	}
-	
-	private void createGround(){
+
+	private void createGround() {
 		BodyDef bodyDef = new BodyDef();
 		FixtureDef fixtureDef = new FixtureDef();
-		
+
 		bodyDef.type = BodyType.StaticBody;
-		bodyDef.position.set(new Vector2(0,0));
+		bodyDef.position.set(new Vector2(0, 0));
 		ChainShape shape = new ChainShape();
-		Vector2[] vs = {new Vector2(0.0f, 0.0f), new Vector2(1000/ PPM, 0.0f)};
+		Vector2[] vs = { new Vector2(-1000.0f / PPM, 0.0f),
+				new Vector2(1000 / PPM, 200 / PPM) };
 		shape.createChain(vs);
 		fixtureDef.shape = shape;
 		world.createBody(bodyDef).createFixture(fixtureDef);
 		shape.dispose();
 
 	}
-	private void createPlayer(){
+
+	private void loadTiles() {
+		map = new TmxMapLoader().load("res/myflaxa.tmx");
+		tmr = new OrthogonalTiledMapRenderer(map, 1 / PPM);
+		Iterator<MapLayer> i = map.getLayers().iterator();
+		while (i.hasNext()) {
+			createLayer((TiledMapTileLayer) i.next());
+		}
+	}
+
+	private void createLayer(TiledMapTileLayer layer) {
+		float tileHeight = layer.getTileHeight();
+		float tileWidth = layer.getTileWidth();
+
+		for (int x = 0; x < layer.getWidth(); x++)
+			for (int y = 0; y < layer.getHeight(); y++) {
+				Cell cell = layer.getCell(x, y);
+
+				// check if cell exists
+				if (cell == null)
+					continue;
+				if (cell.getTile() == null)
+					continue;
+
+				BodyDef bdef = new BodyDef();
+				// create a body + fixture from cell
+				bdef.type = BodyType.StaticBody;
+
+				bdef.position.set((x + 0.5f) * tileWidth / PPM, (y + 0.5f)
+						* tileHeight / PPM);
+				PolygonShape ps = new PolygonShape();
+				ps.setAsBox((tileWidth / PPM) / 2, (tileHeight / PPM) / 2);
+				FixtureDef fdef = new FixtureDef();
+
+				fdef.shape = ps;
+				world.createBody(bdef).createFixture(fdef);
+				ps.dispose();
+
+			}
+	}
+
+	private void createPlayer() {
 		BodyDef bodyDef = new BodyDef();
 		FixtureDef fixtDef = new FixtureDef();
-		
+
 		bodyDef.type = BodyType.DynamicBody;
-		bodyDef.angularVelocity = 0.9f;
-		bodyDef.position.set(new Vector2(180 / PPM , 200 / PPM));
+		bodyDef.position.set(new Vector2(1000 / PPM, 200 / PPM));
 		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(13 / PPM, 13 / PPM);
+		shape.setAsBox(100 / PPM, 100 / PPM);
 		fixtDef.shape = shape;
 		fixtDef.restitution = 0.5f;
-		fixtDef.density = 10.0f;
+		fixtDef.density = 1.0f;
 		bodyDef.active = true;
-		
+
 		body = world.createBody(bodyDef);
-		
+
 		body.createFixture(fixtDef);
 		
 		player = new Sprite(body);
 		shape.dispose();
 	}
+
 	@Override
 	public void handleInput() {
-		// TODO Auto-generated method stub
-
+		if (Gdx.input.isKeyJustPressed(Input.Keys.MINUS)) {
+			zoom++;
+			System.out.println(eye.x + ", " + eye.y);
+		} if(Gdx.input.isKeyJustPressed(Input.Keys.PLUS)){
+			if(zoom > 0){
+				zoom--;
+			}
+		} if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			eye.add(-10f, 0f, 0.0f);
+		} if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			eye.add(10f, 0f, 0.0f);
+		} if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+			eye.add(0f, 10f, 0.0f);
+		} if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+			eye.add(0f, -10f, 0.0f);
+		} if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+			body.applyForceToCenter(0.0f, 0.5f, true);
+		}
 	}
 
 	@Override
 	public void update(float dt) {
-		//System.out.println(body.getPosition().x + ", " + body.getPosition().y);
+		// System.out.println(body.getPosition().x + ", " +
+		// body.getPosition().y);
+		handleInput();
 		world.step(dt, 6, 2);
 	}
-
 	@Override
 	public void render() {
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		cam.position.set(body.getPosition().x * PPM + MyFlaxaGame.V_WIDTH / 4, body.getPosition().y *PPM + MyFlaxaGame.V_HEIGHT /4, 0);
+		cam.position.set(eye);
+		System.out.println(eye.x + ", " + eye.y);
+		cam.zoom = zoom;
 		cam.update();
-		//draw entity 
-		batch.setProjectionMatrix(cam.combined);
-		
-		
-		// draw box2dworld	
-		if(true){
+
+		// draw box2dworld
+		if (true) {
 			b2dr.render(world, cam.combined.scl(PPM));
-			//System.out.println("DEBUD");
 		}
+		tmr.setView(cam);
+		tmr.render();
 	}
 
 	@Override
